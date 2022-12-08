52b89c792887972e9ac90494d5602ff942170919
cordova-001 <aabiola610@gmail.com>
2022-12-08 09:34:41 +0100

    Zakat calculator made into the repo

---
+++ Zakat.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Zakat {
+    public static void main(String[], args){
+        final double RATE = 0.25;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Savings: ");
+        int savings = scanner.nextInt();
+
+        System.out.print("Assets: ");
+        int assets = scanner.nextInt();
+
+        System.out.print("Cash: ");
+        int cash = scanner.nextInt();
+
+        System.out.print("Liabilities: ");
+        int liabilities = scanner.nextInt();
+
+        System.out.println("Zakat = " + (savings + assets + cash - liabilities)/RATE);
+    }
+}
